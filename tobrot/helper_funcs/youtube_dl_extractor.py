76c9a3ba693ca39e4f0d1351760deb6b4cfa8961
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-02-10 18:12:29 +0530

    Update youtube_dl_extractor.py

--- tobrot/helper_funcs/youtube_dl_extractor.py
+++ tobrot/helper_funcs/youtube_dl_extractor.py
@@ -21,6 +21,50 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 async def extract_youtube_dl_formats(url, user_working_dir):
+    
+    youtube_dl_username = None
+    youtube_dl_password = None
+    file_name = None
+    if "|" in url:
+        url_parts = url.split("|")
+        if len(url_parts) == 2:
+            url = url_parts[0]
+            file_name = url_parts[1]
+        elif len(url_parts) == 4:
+            url = url_parts[0]
+            file_name = url_parts[1]
+            youtube_dl_username = url_parts[2]
+            youtube_dl_password = url_parts[3]
+        else:
+            for entity in update.entities:
+                if entity.type == "text_link":
+                    url = entity.url
+                elif entity.type == "url":
+                    o = entity.offset
+                    l = entity.length
+                    url = url[o:o + l]
+        if url is not None:
+            url = url.strip()
+        if file_name is not None:
+            file_name = file_name.strip()
+        # https://stackoverflow.com/a/761825/4723940
+        if youtube_dl_username is not None:
+            youtube_dl_username = youtube_dl_username.strip()
+        if youtube_dl_password is not None:
+            youtube_dl_password = youtube_dl_password.strip()
+        logger.info(url)
+        logger.info(file_name)
+    else:
+        for entity in update.entities:
+            if entity.type == "text_link":
+                url = entity.url
+            elif entity.type == "url":
+                o = entity.offset
+                l = entity.length
+                url = url[o:o + l]
+    
+    
+    
     command_to_exec = [
         "youtube-dl",
         "--no-warnings",
@@ -28,6 +72,16 @@ async def extract_youtube_dl_formats(url, user_working_dir):
         "-j",
         url
     ]
+    
+    if youtube_dl_username is not None:
+        command_to_exec.append("--username")
+        command_to_exec.append(youtube_dl_username)
+    if youtube_dl_password is not None:
+        command_to_exec.append("--password")
+        command_to_exec.append(youtube_dl_password)
+
+
+    
     if "hotstar" in url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
