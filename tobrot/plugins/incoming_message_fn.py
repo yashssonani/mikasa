298aed4ef5c3344ae165efdcd404849f6a61f6c2
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-12 21:31:59 +0530

    Add files via upload

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -31,9 +31,12 @@ async def incoming_message_f(client, message):
     """/leech command"""
     i_m_sefg = await message.reply_text("processing", quote=True)
     is_zip = False
+    is_unzip = False
     if len(message.command) > 1:
         if message.command[1] == "archive":
             is_zip = True
+        elif message.command[1] == "unzip":
+            is_unzip = True    
     # get link from the incoming message
     dl_url, cf_name = extract_link(message.reply_to_message)
     LOGGER.info(dl_url)
@@ -48,7 +51,8 @@ async def incoming_message_f(client, message):
         new_download_location = os.path.join(
             DOWNLOAD_LOCATION,
             str(current_user_id),
-            str(time.time())
+            str(time.time()),
+            cf_name if cf_name is not None else ""
         )
         # create download directory, if not exist
         if not os.path.isdir(new_download_location):
@@ -60,7 +64,8 @@ async def incoming_message_f(client, message):
             dl_url,
             new_download_location,
             i_m_sefg,
-            is_zip
+            is_zip,
+            is_unzip
         )
         if not sagtus:
             # if FAILED, display the error message
