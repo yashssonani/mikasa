1db3bef53efe1a3e169fde4ec279c856cc1f52b4
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-12 15:33:10 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -254,7 +254,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 # quote=True,
                 thumb=thumb,
                 caption=local_file_name, #caption_str,
-                parse_mode="html",
+                #parse_mode="html",
                 disable_notification=True,
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
