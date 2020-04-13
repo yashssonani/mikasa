bb99569d8219326e6ef5fe4de0660452566413a2
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-13 18:17:36 +0530

    Update remove_words.py

--- tobrot/helper_funcs/remove_words.py
+++ tobrot/helper_funcs/remove_words.py
@@ -21,31 +21,19 @@ from tobrot import (
 
 )
 async def remove_w(yo):
-      return_name = "hello.mkv"
-      #uj = "hell" "mojo"
-      a = yo #"@GTMovise Orange.is.the.New.Black.S01E04.720p.BrRip.Hin-Eng.x265.HEVC-KatmovieHD.nl [Telly].mkv"
-      r_word = REMOVE_WORD
-      #print(AUTH_USERS)
-      r_word = r_word.split()
-      #print(AUTH_USERS)
-      #yy = chr(5)
-      #print(yy)
-      cars = []
-      cars = cars + r_word
-      #print(cars)
-      #TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-      for b in cars:
-            if b in a:
-                  c = a.replace(b, "")
-                  a=c
-      c = a
+       a = yo
+       #b = ['KatmovieHD']
+       b = REMOVE_WORD.split()
+       for bya in b:
+             if bya in a:
+                 ko = a.replace(bya, "")
+                 a = ko
+       a=ko 
+       a=a.split()
+       a=" ".join(a)
+       
+       #print(a)
+       return a;
 
 
-      if c is none:
-         return_name = return_name 
-      else:
-         return_name = c
-
-            #yoki = ' '.join(c.split())
-  return return_name
+      
