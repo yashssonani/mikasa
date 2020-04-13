59bf283ef3135f0f80e6dfc5a68104cc04cec4fe
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-13 17:38:05 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -262,7 +262,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 document=local_file_name,
                 # quote=True,
                 thumb=thumb,
-                caption=REMOVE_WORD#caption_str, #caption_str,
+                caption=REMOVE_WORD,#caption_str, #caption_str,
                 #parse_mode="html",
                 disable_notification=True,
                 reply_to_message_id=message.reply_to_message.message_id,
