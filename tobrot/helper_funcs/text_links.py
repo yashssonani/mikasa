dc3dffe20151bf7b709e14c166fd04d81e393c53
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-17 21:48:27 +0530

    Update text_links.py

--- tobrot/helper_funcs/text_links.py
+++ tobrot/helper_funcs/text_links.py
@@ -6,3 +6,20 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
+import asyncio
+import os
+import time
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from PIL import Image
+from tobrot.helper_funcs.display_progress import progress_for_pyrogram, humanbytes
+from tobrot.helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
+from tobrot.helper_funcs.split_large_files import split_large_files
+from tobrot.helper_funcs.copy_similar_file import copy_file
+from tobrot.helper_funcs.remove_words import remove_w
+from tobrot import (
+    TG_MAX_FILE_SIZE,
+    EDIT_SLEEP_TIME_OUT,
+    DOWNLOAD_LOCATION,
+    REMOVE_WORD
+)
