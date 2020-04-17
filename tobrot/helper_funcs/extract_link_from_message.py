9dd2fd3bd66423bd29f604a774b5f0e7ac485983
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-17 22:32:54 +0530

    Update extract_link_from_message.py

--- tobrot/helper_funcs/extract_link_from_message.py
+++ tobrot/helper_funcs/extract_link_from_message.py
@@ -12,16 +12,21 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 
-def extract_link(message):
+def extract_link(message, is_multi, download_location):
     custom_file_name = None
     url = None
-    if message is None:
-        url = None
-        custom_file_name = None
-    elif "|" in message.text:
-        url, custom_file_name = message.text.split("|")
-        url = url.strip()
-        custom_file_name = custom_file_name.strip()
-    elif message.entities is not None:
-        url = message.text
+    if is_multi:
+       url = multi_links(message, download_location)
+    else:
+        if message is None:
+           url = None
+           custom_file_name = None
+        elif "|" in message.text:
+           url, custom_file_name = message.text.split("|")
+           url = url.strip()
+           custom_file_name = custom_file_name.strip()
+        elif message.entities is not None:
+           url = message.text
     return url, custom_file_name
+
+    
