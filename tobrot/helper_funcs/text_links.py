fd6ad8d6b9ac3e67fd09372f38e533e9c8168df2
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-17 21:45:58 +0530

    Update text_links.py

--- tobrot/helper_funcs/text_links.py
+++ tobrot/helper_funcs/text_links.py
@@ -1,1 +1,8 @@
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
 
