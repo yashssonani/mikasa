77f4ec8f9a1316bbe73a09a107f51618477145e0
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-18 23:15:48 +0530

    Add files via upload

--- tobrot/helper_funcs/extract_link_from_message.py
+++ tobrot/helper_funcs/extract_link_from_message.py
@@ -10,25 +10,28 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
-
+from pyrogram import Client
+from tobrot import (
+  DOWNLOAD_LOCATION
+  )
 
 def extract_link(message):
     custom_file_name = None
     url = None
-    if message.document
-       url = None
-       custom_file_name = None
-
-    else:
-        if message is None:
-           url = None
-           custom_file_name = None
-        elif "|" in message.text:
-           url, custom_file_name = message.text.split("|")
-           url = url.strip()
-           custom_file_name = custom_file_name.strip()
-        elif message.entities is not None:
-           url = message.text
+    if message is None:
+        url = None
+        custom_file_name = None
+        """
+    elif message.document:
+        Client.download_media(
+          message = message,
+          file_name = DOWNLOAD_LOCATION
+          )
+          """
+    elif "|" in message.text:
+        url, custom_file_name = message.text.split("|")
+        url = url.strip()
+        custom_file_name = custom_file_name.strip()
+    elif message.entities is not None:
+        url = message.text
     return url, custom_file_name
-
-    
