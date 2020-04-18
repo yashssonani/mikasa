b893285f2f8c27a24c18ebf7affe06739f133dca
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-18 23:13:42 +0530

    Add files via upload

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -18,14 +18,13 @@ from tobrot import (
     DOWNLOAD_LOCATION
 )
 
-
+import pyrogram
 import time
 from tobrot.helper_funcs.extract_link_from_message import extract_link
 from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
 from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
-#from tobrot.helper_funcs.text_links import multi_links
 
 
 async def incoming_message_f(client, message):
@@ -33,49 +32,88 @@ async def incoming_message_f(client, message):
     i_m_sefg = await message.reply_text("processing", quote=True)
     is_zip = False
     is_unzip = False
-    is_multi = False
     if len(message.command) > 1:
         if message.command[1] == "archive":
             is_zip = True
-        elif message.command[1] == "multi":
-            is_multi = True 
         elif message.command[1] == "unzip":
             is_unzip = True
-   
     # get link from the incoming message
-    dl_url, cf_name = extract_link(message.reply_to_message, is_multi)
-    LOGGER.info(dl_url)
-    LOGGER.info(cf_name)
-    if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
+    if message.reply_to_message.document:
+      hell = await message.reply_to_message.download(
+        file_name = DOWNLOAD_LOCATION
+        )
+      i_m_sefg = await message.reply_text(text=hell, quote=True)
+      with open (hell) as foe:
+        for rec in foe:
+          dl_url = rec
+          cf_name = None
+          is_zip = False
+          is_unzip = False
+          LOGGER.info(dl_url)
+          LOGGER.info(cf_name)
+          if dl_url is not None:
+              await i_m_sefg.edit_text("extracting links")
+              aria_i_p = await aria_start()
+              LOGGER.info(aria_i_p)
+              current_user_id = message.from_user.id
+              new_download_location = os.path.join(
+                DOWNLOAD_LOCATION,
+                str(current_user_id),
+                str(time.time())
+              )
+              if not os.path.isdir(new_download_location):
+                  os.makedirs(new_download_location)
+              await i_m_sefg.edit_text("trying to download")
+       
+              sagtus, err_message = await call_apropriate_function(
+                  aria_i_p,
+                  dl_url,
+                  new_download_location,
+                  i_m_sefg,
+                  is_zip,
+                  is_unzip
+              )
+              if not sagtus:
+                # if FAILED, display the error message
+                  await i_m_sefg.edit_text(err_message)
+          else:
+              await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
+    else:
+      i_m_sefg = await message.reply_text("is not", quote=True)
+      dl_url, cf_name = extract_link(message.reply_to_message)
+      LOGGER.info(dl_url)
+      LOGGER.info(cf_name)
+      if dl_url is not None:
+          await i_m_sefg.edit_text("extracting links")
         # start the aria2c daemon
-        aria_i_p = await aria_start()
-        LOGGER.info(aria_i_p)
-        current_user_id = message.from_user.id
+          aria_i_p = await aria_start()
+          LOGGER.info(aria_i_p)
+          current_user_id = message.from_user.id
         # create an unique directory
-        new_download_location = os.path.join(
+          new_download_location = os.path.join(
             DOWNLOAD_LOCATION,
             str(current_user_id),
             str(time.time())
-        )
+          )
+          y
         # create download directory, if not exist
-        if not os.path.isdir(new_download_location):
-            os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
+          if not os.path.isdir(new_download_location):
+              os.makedirs(new_download_location)
+          await i_m_sefg.edit_text("trying to download")
         # try to download the "link"
-        sagtus, err_message = await call_apropriate_function(
-            aria_i_p,
-            dl_url,
+          sagtus, err_message = await call_apropriate_function(
+              aria_i_p,
+              dl_url,
             new_download_location,
-            i_m_sefg,
-            is_zip,
-            is_unzip
-        )
-        if not sagtus:
+              i_m_sefg,
+              is_zip,
+              is_unzip
+          )
+          if not sagtus:
             # if FAILED, display the error message
-            await i_m_sefg.edit_text(err_message)
-    else:
-        await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
+              await i_m_sefg.edit_text(err_message)
+      else:
+          await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
 
 
 async def incoming_youtube_dl_f(client, message):
