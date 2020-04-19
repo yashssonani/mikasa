e73498b34cce2c9e07f6431a01c483568eec638b
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-19 18:52:01 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -131,7 +131,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
         message_for_progress_display = await message.reply_text(
             "starting upload of {}".format(os.path.basename(local_file_name))
         )
-        if local_file_name.upper().endswith(("MKV","MP4","WEBM")):
+        if local_file_name.upper().endswith(("AVI")):
             metadata = extractMetadata(createParser(local_file_name))
             duration = 0
             if metadata.has("duration"):
