d8691d31ea418a5b3f42865bc178e097593a0c12
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-05-16 22:49:50 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -278,7 +278,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 local_file_name = os.rename(local_file_name, yash)
                 local_file_name = yash
                 #hell = asyncio.run(url_up(local_file_name))
-                caption_str = caption_str2 + #hell
+                caption_str = caption_str2 #+ hell
             sent_message = await message.reply_document(
                 document=local_file_name,
                 # quote=True,
