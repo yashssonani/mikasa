bfc46c6b41cdb7cb93340166ab4beeacfc8b4714
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-05-31 17:05:59 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -122,13 +122,13 @@ async def incoming_message_f(client, message):
                url
           ]
           process = call(command, shell=False)
-          if unzip:
+          if is_unzip:
             new_download_location = await create_unzip(new_download_location)
-              to_upload_file = new_download_location
-              response = {}
-              LOGGER.info(response)
-              user_id = sent_message_to_update_tg_p.reply_to_message.from_user.id
-              final_response = await upload_to_tg(sent_message_to_update_tg_p,to_upload_file,user_id,response)
+            to_upload_file = new_download_location
+            response = {}
+            LOGGER.info(response)
+            user_id = sent_message_to_update_tg_p.reply_to_message.from_user.id
+            final_response = await upload_to_tg(sent_message_to_update_tg_p,to_upload_file,user_id,response)
           else:
             to_upload_file = new_download_location
             response = {}
